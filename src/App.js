cbf54f3d2cea169a6c2bba31a3fe8565cf99b9d5
Ernis1015 <156911113+Ernis1015@users.noreply.github.com>
2024-06-04 17:47:17 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import UserForm from "./components/userForm/UserForm";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User Management</h1>
+      <UserForm/>
+    </div>
+  );
+}
+
+export default App;
